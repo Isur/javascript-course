97d97e9a6d80c052afead6efb0d5b272af10a57c
Isur <bednarczykartur96@gmail.com>
2018-07-05 09:27:08 +0200

    mockapi working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger; // eslint-disable-line no-debugger 
@@ -18,4 +18,18 @@ getUsers().then(result =>{
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event){
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
+
+
